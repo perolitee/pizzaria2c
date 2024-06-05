db41c2180ab273c8b4cf54c2a69d65eba261112a
perolitee <perolaozacarioto@gmail.com>
2024-06-05 11:04:41 -0300

    rotas

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/react.svg'
+
+const Home = () => {
+
+    return (
+        <img src={logo} width="300"
+        alt="imagem do logo react" />
+    )
+}
+
+export default Home 
